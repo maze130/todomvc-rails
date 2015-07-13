d28c9bc4449911ebbb5f45f9ea20f0773834a7bb
Jason Weinberger & Vic Zhu <pair+maze130.sayuloveit@devbootcamp.com>
2015-07-13 12:09:56 -0400

    Initial commit with working List attributes

---
+++ todo/app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.order(:id)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to lists_path
+  end
+
+  def show
+    @list = List.find_by(id: params[:id])
+  end
+
+  def edit
+    @list = List.find_by(id: params[:id])
+  end
+
+  def update
+    List.update(params[:id], list_params)
+    redirect_to lists_path
+  end
+
+  def destroy
+    List.destroy(params[:id])
+    redirect_to lists_path
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
